487d20d69617a7b71499e4d2753612e6d70e37e2
julian hahn <julianhahn01@gmail.com>
2023-08-21 09:02:44 +0900

    current svelte setup

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,40 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	let puppy: string;
+	let loading = false;
+
+	async function loadPupppy() {
+		loading = true;
+		const res = await fetch('https://placedog.net/500/280');
+		puppy = URL.createObjectURL(await res.blob());
+		loading = false;
+	}
+</script>
+
+<main>
+	<button on:click={loadPupppy}> click for puppy </button>
+	{#if loading}
+		<p>loading...</p>
+	{:else if puppy}
+		<img src={puppy} alt="puppy" />
+	{/if}
+</main>
+
+<style style="scss">
+	.main {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		height: 100%;
+		flex-direction: column;
+		font-family: 'Roboto', sans-serif;
+		font-size: 24px;
+	}
+
+	button {
+		padding: 1rem 2rem;
+		border: 1px solid #ccc;
+		border-radius: 5px;
+		font-size: 1.5rem;
+		cursor: pointer;
+	}
+</style>
